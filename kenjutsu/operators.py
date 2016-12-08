c5469df70ffe49677000d47d6ba0ed97df76d5b0
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-12-08 16:20:14 -0500

    Add a function to join two slices together.

--- kenjutsu/operators.py
+++ kenjutsu/operators.py
@@ -1,4 +1,67 @@
+from __future__ import absolute_import
+
+
 __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Dec 08, 2016 16:20:14 GMT-0500$"
 
 
+import kenjutsu.format
+import kenjutsu.measure
+
+
+def join_slice(slice_1, slice_2, length=None):
+    """
+        Joins the two slices into one.
+
+        Applying the resulting slice should have the same effect as applying
+        the two slices in order.
+
+        Args:
+            slice_1(slice):                   first slice to apply
+            slice_2(slice):                   second slice to apply
+            length(int):                      length of each dimension of the
+                                              object to slice.
+
+        Returns:
+            (slice):                          one slice equivalent to applying
+                                              the two slices in order.
+
+        Examples:
+
+            >>> join_slice(slice(2, None), slice(None, 10))
+            slice(2, 10, 1)
+    """
+
+    rf_slice_1 = kenjutsu.format.reformat_slice(slice_1, length)
+
+    len_slice_1 = None
+    try:
+        len_slice_1 = kenjutsu.measure.len_slice(rf_slice_1)
+    except kenjutsu.measure.UnknownSliceLengthException:
+        pass
+
+    rf_slice_2 = kenjutsu.format.reformat_slice(slice_2, len_slice_1)
+
+    result_slice_step = rf_slice_1.step * rf_slice_2.step
+    if result_slice_step > 0:
+        result_slice_start = rf_slice_1.start + rf_slice_2.start
+        if rf_slice_1.stop is None:
+            result_slice_stop = rf_slice_2.stop
+        elif rf_slice_2.stop is None:
+            result_slice_stop = rf_slice_1.stop
+        else:
+            result_slice_stop = rf_slice_1.stop - rf_slice_2.stop
+    else:
+        result_slice_start = rf_slice_1.start - rf_slice_2.start
+        if rf_slice_1.stop is None:
+            result_slice_stop = rf_slice_2.stop
+        elif rf_slice_2.stop is None:
+            result_slice_stop = rf_slice_1.stop
+        else:
+            result_slice_stop = rf_slice_1.stop + rf_slice_2.stop
+
+    result_slice = slice(result_slice_start,
+                         result_slice_stop,
+                         result_slice_step)
+
+    return result_slice
--- tests/test_operators.py
+++ tests/test_operators.py
@@ -2,4 +2,130 @@ __author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
 __date__ = "$Dec 08, 2016 16:20:14 GMT-0500$"
 
 
+import doctest
+import itertools
+import math
+import operator
+import unittest
+
 from kenjutsu import operators
+
+
+try:
+    irange = xrange
+except NameError:
+    irange = range
+
+
+# Load doctests from `operators`.
+def load_tests(loader, tests, ignore):
+    tests.addTests(doctest.DocTestSuite(operators))
+    return tests
+
+
+class TestOperators(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+    def test_reformat_slice(self):
+        for size in [10, 11, 12]:
+            excess = size + 3
+            each_range = range(size)
+
+            for start_1, start_2 in itertools.product(*itertools.tee(
+                    itertools.chain([None], irange(-excess, excess)))):
+                for stop_1, stop_2 in itertools.product(*itertools.tee(
+                        itertools.chain([None], irange(-excess, excess)))):
+                    for step_1, step_2 in itertools.product(*itertools.tee(
+                            irange(-excess, excess))):
+                        step_1 = None if step_1 == 0 else step_1
+                        step_2 = None if step_2 == 0 else step_2
+
+                        slice_1 = slice(start_1, stop_1, step_1)
+                        slice_2 = slice(start_2, stop_2, step_2)
+
+                        j_slice = operators.join_slice(slice_1, slice_2)
+                        self.assertEqual(
+                            each_range[slice_1][slice_2],
+                            each_range[j_slice]
+                        )
+
+                        j_slice = operators.join_slice(slice_1, slice_2, size)
+                        self.assertEqual(
+                            each_range[slice_1][slice_2],
+                            each_range[j_slice]
+                        )
+
+                        new_start = j_slice.start
+                        new_stop = j_slice.stop
+                        new_step = j_slice.step
+
+                        if (new_step is not None and
+                            new_step < 0 and
+                            new_stop is None):
+                            new_stop = -1
+
+                        l = float(new_stop - new_start)/float(new_step)
+                        self.assertEqual(
+                            int(math.ceil(l)),
+                            len(each_range[slice_1][slice_2])
+                        )
+
+                        a_slice = list()
+                        a_slice.append(0 if start is None else start)
+                        a_slice.append(0 if stop is None else stop)
+                        a_slice.append(0 if step is None else step)
+
+                        a_op = operator.itemgetter(*a_slice)
+
+                        expected_result = None
+                        try:
+                            expected_result = a_op(each_range)
+                        except IndexError:
+                            pass
+
+                        if expected_result is not None:
+                            rf_slice = operators.reformat_slice(a_slice)
+                            rf_op = operator.itemgetter(*rf_slice)
+                            self.assertEqual(
+                                expected_result,
+                                rf_op(each_range)
+                            )
+
+                            rf_slice = operators.reformat_slice(a_slice, size)
+                            rf_op = operator.itemgetter(*rf_slice)
+                            self.assertEqual(
+                                expected_result,
+                                rf_op(each_range)
+                            )
+                        else:
+                            operators.reformat_slice(a_slice)
+                            with self.assertRaises(IndexError):
+                                operators.reformat_slice(a_slice, size)
+
+                if start is not None:
+                    a_slice = start
+
+                    expected_result = None
+                    try:
+                        expected_result = each_range[a_slice]
+                    except IndexError:
+                        pass
+
+                    if expected_result is not None:
+                        rf_slice = operators.reformat_slice(a_slice)
+                        self.assertEqual(
+                            expected_result,
+                            each_range[rf_slice]
+                        )
+
+                        rf_slice = operators.reformat_slice(a_slice, size)
+                        self.assertEqual(
+                            expected_result,
+                            each_range[rf_slice]
+                        )
+                    else:
+                        operators.reformat_slice(a_slice)
+                        with self.assertRaises(IndexError):
+                            operators.reformat_slice(a_slice, size)
