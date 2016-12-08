8bb081561429668b1c561de85ea5445a2447625a
John Kirkham <kirkhamj@janelia.hhmi.org>
2016-12-08 11:38:44 -0500

    Re-add stubs for old `kenjutsu.kenjutsu`.

---
+++ kenjutsu/kenjutsu.py
@@ -0,0 +1,4 @@
+__author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
+__date__ = "$Sep 08, 2016 15:46:46 EDT$"
+
+
---
+++ tests/test_kenjutsu.py
@@ -0,0 +1,4 @@
+__author__ = "John Kirkham <kirkhamj@janelia.hhmi.org>"
+__date__ = "$Oct 02, 2016 15:38:07 EDT$"
+
+
